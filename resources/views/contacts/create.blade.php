94354c0733cb5335ec0effd5ba8713dcd5b094b6
Felipe Aurelio <felipeaurelio@pop-os.localdomain>
2020-06-08 14:19:15 -0300

    Initial commit

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h1>Adicionar Novo Produto</h1>
+    <form action="/products" method="post">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="title">Nome do Produto</label>
+            <input type="text" class="form-control" id="productName" name="name">
+        </div>
+        <div class="form-group">
+            <label for="description">Fabricante</label>
+            <select class="form-control" name="manufactory">
+                <option>Apple</option>
+                <option>Google</option>
+                <option>LG</option>
+                <option>Samsung</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Quantidade</label>
+            <input type="text" class="form-control" id="productAmount" name="amount"/>
+        </div>
+        <div class="form-group">
+            <label for="description">Produto Diponível</label><br/>
+            <label class="radio-inline"><input type="radio" name="available" value="1"> Sim</label>
+            <label class="radio-inline"><input type="radio" name="available" value="0"> Não</label>
+        </div>
+        <div class="form-group">
+            <label for="description">Descrição</label>
+            <textarea type="text" class="form-control" id="description" name="description"></textarea>
+        </div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+    @if (Session::has('message'))
+        <div class="alert alert-info">{{ Session::get('message') }}</div>
+    @endif
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome do Produto</th>
+            <th scope="col">Descrição do Produto</th>
+            <th scope="col">Fabricante</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Disponível</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($products as $product)
+            <tr>
+                <th scope="row">{{$product->id}}</th>
+                <td>{{$product->name}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->manufactory}}</td>
+                <td>{{$product->amount}}</td>
+                <td>{{$product->available ? 'Sim' : 'Não'}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
