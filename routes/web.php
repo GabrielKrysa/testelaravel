8394a400f672145e2949f7d43f241e7b008f1e15
Felipe Aurelio <felipeaurelio@pop-os.localdomain>
2020-06-09 09:42:15 -0300

    Adicionado verificacao de autenticacao nos Controllers de produto e contato.

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('contacts/create', 'ContactController@create');
 Route::post('contacts', 'ContactController@show');
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/products/create', 'ProductController@create');
 Route::post('/products', 'ProductController@store');
 Route::get('/products', 'ProductController@index');
