5d6fe1845cf95fa4935fc506ddc8d92e694e0b83
Felipe Aurelio <felipeaurelio@pop-os.localdomain>
2020-06-09 16:08:35 -0300

    Mudado para portugues telas do fluxo de login.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,11 +40,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">Entrar</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">Cadastrar</a>
                                 </li>
                             @endif
                         @else
@@ -57,7 +57,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        Sair
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('', 'BookController@index');
 Route::get('contacts/create', 'ContactController@create');
 Route::post ('contacts', 'ContactController@show');
 Route::get('/products/create', 'ProductController@create');
 Route::post('/products', 'ProductController@store');
 Route::get('/products', 'ProductController@index');
 
+Route::get('', 'BookController@index');
+Route::get('home', 'BookController@index');
 Route::get('books/create', 'BookController@create');
 Route::get('books', 'BookController@index')->name('books.index');
 Route::post('books', 'BookController@store');
