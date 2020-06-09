e2b8d9b9dd477f131e4b92c29ebf35e780fc816b
Felipe Aurelio <felipeaurelio@pop-os.localdomain>
2020-06-09 16:24:47 -0300

    Aplicado Accessors e Mutators nos campos de dono e autor do livro para UpperCase na primeira letra de cada nome.

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $guarded = [];
+    protected $fillable = ['title', 'author'];
+
+    public function getAuthorAttribute($value)
+    {
+        return ucwords($value);
+    }
+
+    public function getOwnerAttribute($value)
+    {
+        return ucwords($value);
+    }
+
+    public function setAuthorAttribute($value)
+    {
+        $this->attributes['author'] = ucwords($value);
+    }
+
+    public function setOwnerAttribute($value)
+    {
+        $this->attributes['owner'] = ucwords($value);
+    }
 }
