3699221e702086d8b442790b2af09b1a94db278b
Felipe Aurelio <felipeaurelio@pop-os.localdomain>
2020-06-10 09:02:01 -0300

    Removido arquivos inutilizados.

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('contacts/create', 'ContactController@create');
-Route::post ('contacts', 'ContactController@show');
-Route::get('/products/create', 'ProductController@create');
-Route::post('/products', 'ProductController@store');
-Route::get('/products', 'ProductController@index');
-/*
 
-Route::get('books/create', 'BookController@create');
-Route::get('books', 'BookController@index')->name('books.index');
-Route::post('books', 'BookController@store');
-Route::get('books/edit/{id}', 'BookController@edit')->name('books.edit');
-Route::get('books/show/{id}', 'BookController@show')->name('books.show');
-Route::post('books/edit/{id}', 'BookController@update')->name('books.update');
-Route::delete('books/delete/{id}', 'BookController@destroy')->name('books.destroy');
-*/
 Route::resource('books', 'BookController');
-Route::get('', 'BookController@index');
-Route::get('home', 'BookController@index');
+Route::get('/', 'BookController@index');
+Route::get('/home', 'BookController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
